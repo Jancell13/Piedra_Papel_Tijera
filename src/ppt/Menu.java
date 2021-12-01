559727d5c18fbb45980baef1554349207eb3fe33
Jancell <71906690+Jancell13@users.noreply.github.com>
2021-11-30 21:40:09 -0500

    Update Menu.java

--- src/ppt/Menu.java
+++ src/ppt/Menu.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ppt;
 
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 
-/**
- *
- * @author acer
- */
 public class Menu extends javax.swing.JFrame {
 
     /**
